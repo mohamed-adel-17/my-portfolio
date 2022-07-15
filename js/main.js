d07336386ca355fe8c17ca45406bbb9a149fdd8e
Mohamed Adel Fahmi <90067715+mohamed-adel-17@users.noreply.github.com>
2022-07-15 02:27:05 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,70 @@
+$("#home").click(function(){
+    $(".rightSide1").show(600)
+    $(".rightSide2").hide(400)
+    $(".rightSide3").hide(400)
+    $(".rightSide4").hide(400)
+    $(".rightSide5").hide(400)
+    $(".rightSide6").hide(400)
+    // $(".leftSide").hide(300)
+})
+
+$(".logo").click(function(){
+    $(".rightSide1").show(600)
+    $(".rightSide2").hide(400)
+    $(".rightSide3").hide(400)
+    $(".rightSide4").hide(400)
+    $(".rightSide5").hide(400)
+    $(".rightSide6").hide(400)
+})
+
+
+$("#about").click(function () {
+    $(".rightSide2").toggle(600)
+    $(".rightSide1").hide(400)
+    $(".rightSide3").hide(400)
+    $(".rightSide4").hide(400)
+    $(".rightSide5").hide(400)
+    $(".rightSide6").hide(400)
+})
+
+$("#skills").click(function () {
+    $(".rightSide2").hide(400)
+    $(".rightSide1").hide(400)
+    $(".rightSide3").toggle(600)
+    $(".rightSide4").hide(400)
+    $(".rightSide5").hide(400)
+    $(".rightSide6").hide(400)
+})
+
+$("#services").click(function () {
+    $(".rightSide2").hide(400)
+    $(".rightSide1").hide(400)
+    $(".rightSide3").hide(400)
+    $(".rightSide4").toggle(600)
+    $(".rightSide5").hide(400)
+    $(".rightSide6").hide(400)
+})
+
+$("#works").click(function () {
+    $(".rightSide2").hide(400)
+    $(".rightSide1").hide(400)
+    $(".rightSide3").hide(400)
+    $(".rightSide4").hide(400)
+    $(".rightSide5").toggle(600)
+    $(".rightSide6").hide(400)
+})
+
+$("#contacts").click(function () {
+    $(".rightSide2").hide(400)
+    $(".rightSide1").hide(400)
+    $(".rightSide3").hide(400)
+    $(".rightSide4").hide(400)
+    $(".rightSide5").hide(400)
+    $(".rightSide6").toggle(600)
+})
+
+
+
+
+
+
